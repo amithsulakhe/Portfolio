e8f91abd868e470c340361fc34a22be85589200f
Amith Sulakhe <amithsulakhe2468@gmail.com>
2023-08-01 23:16:39 +0530

    updatedskillsection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const ToggleBtn=document.querySelector(".bars")
 const BarDisplay=document.querySelector(".sub")
+const skill_container=document.querySelector(".skill_container")
 console.log(ToggleBtn);
 // const link=document.querySelector(".fa-brands")
 ToggleBtn.addEventListener("click",funtoggle)
@@ -22,3 +23,48 @@ function FunProfileToggle(link, isMouseEnter) {
 }
 
 
+// Skills 
+let arr=[{
+    skill:["asset 9.png","ReactJS"]
+}
+,{
+    skill:["asset 12.png","Redux"]
+},{
+    skill:["asset 17.png","TailwindCSS"]
+},
+
+{
+    skill:["asset 18.png","Bootstrap"]
+},{
+    skill:["asset 20.png","HTML5"]
+},{
+    skill:["asset 21.png","CSS5"]
+},
+
+{
+    skill:["asset 22.png","Javascript"]
+},
+
+{
+    skill:["asset 26.png","Python"]
+},{
+    skill:["asset 29.png","MySQL"]
+},
+
+{
+    skill:["asset 33.png","Netlify"]
+},
+{
+    skill:["asset 37.png","Github"]
+}
+]
+arr.map((e)=>{
+    let app=document.createElement("div")
+    app.classList.add("app")
+    console.log("./images/"+typeof(e.skill[0]));
+    app.innerHTML=`
+    <img src="./images/${e.skill[0]}"  alt="React">
+ <h3>${e.skill[1]}</h3>
+    `
+    skill_container.appendChild(app)
+})
